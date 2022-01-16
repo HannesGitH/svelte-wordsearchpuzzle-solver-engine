7e7d41209de11eac1d50ba40e3b2fe05dc552187
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-16 12:38:13 +0100

    added comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 import Tess from 'svelte-tesseract';
-import { subscribe } from 'svelte/internal';
 import { derived, writable, Writable, Readable } from 'svelte/store';
 
 export const initialize = async () => {
@@ -11,6 +10,7 @@ export const status = Tess.status;
 /// how far has this step progressed
 export const progress = Tess.progress;
 
+/// the complete map of symbols, no words yet
 export const symbolMap = derived(Tess.result, ($res) => {
 	//TODO: this needs to be more intelligent by creating a bound box etc
 	let shortestLength = Infinity;
@@ -24,7 +24,8 @@ export const symbolMap = derived(Tess.result, ($res) => {
 	return charList.map((row) => row.slice(0, shortestLength));
 });
 
-export const chosenLanguage = writable(<Language>{ id: 'en', name: 'English (US)        ' });
+///the language in which we try to find words
+export const chosenLanguage = writable(<Language>{ id: 'en', name: 'English (US)' });
 
 interface Word {
 	word: string;
@@ -32,8 +33,11 @@ interface Word {
 }
 
 const _foundWords: Writable<Set<Word>> = writable();
+
+/// the actual store to subscribe to
 export const foundWords: Readable<Set<Word>> = { subscribe: _foundWords.subscribe };
 
+/// triggers when language or symbolmap have change to find all words
 const _findWords = derived([symbolMap, chosenLanguage], ([$map, $lang]) => {
 	_foundWords.set(new Set());
 
@@ -51,16 +55,16 @@ const _findWords = derived([symbolMap, chosenLanguage], ([$map, $lang]) => {
 		for (let x1 = 0; x1 < $map.length - 1; x1++) {
 			let currentSymbols: Tesseract.Symbol[] = [$map[x1][y], $map[x1 + 1][y]]; // at least to symbols
 			for (let x2 = x1 + 1; x2 < $map.length; x2++) {
-                currentSymbols.push($map[x2][y]);
+				currentSymbols.push($map[x2][y]);
 				_checkSymbols(currentSymbols, $lang.id);
 			}
 		}
 	}
 
-    //TODO: check diagonals
-
+	//TODO: check diagonals
 });
 
+///helperfunction to check whether a list of symbols is an actual word in the given language
 const _checkSymbols = async (currentSymbols: Tesseract.Symbol[], lang: string) => {
 	const wordstring = currentSymbols.map((sym) => sym.text).reduce((acc, val) => acc + val, '');
 	const api_url = `https://api.dictionaryapi.dev/api/v2/entries/${lang}/${wordstring}`;
@@ -76,21 +80,22 @@ interface Language {
 	name: string;
 }
 
-export const SUPPORTED_LANGUAGES = new Set([
-	<Language>{ id: 'hi', name: 'Hindi               ' },
-	<Language>{ id: 'en', name: 'English (US)        ' },
-	<Language>{ id: 'en-uk', name: 'English (UK)        ' },
-	<Language>{ id: 'es', name: 'Spanish             ' },
-	<Language>{ id: 'fr', name: 'French              ' },
-	<Language>{ id: 'ja', name: 'Japanese            ' },
-	<Language>{ id: 'cs', name: 'Czech               ' },
-	<Language>{ id: 'nl', name: 'Dutch               ' },
-	<Language>{ id: 'sk', name: 'Slovak              ' },
-	<Language>{ id: 'ru', name: 'Russian             ' },
-	<Language>{ id: 'de', name: 'German              ' },
-	<Language>{ id: 'it', name: 'Italian             ' },
-	<Language>{ id: 'ko', name: 'Korean              ' },
-	<Language>{ id: 'pt-BR', name: 'Brazilian Portuguese' },
-	<Language>{ id: 'ar', name: 'Arabic              ' },
-	<Language>{ id: 'tr', name: 'Turkish             ' }
+/// extracted from api.dictionaryapi.dev
+export const SUPPORTED_LANGUAGES = new Set<Language>([
+	{ id: 'hi', name: 'Hindi' },
+	{ id: 'en', name: 'English (US)' },
+	{ id: 'en-uk', name: 'English (UK)' },
+	{ id: 'es', name: 'Spanish' },
+	{ id: 'fr', name: 'French' },
+	{ id: 'ja', name: 'Japanese' },
+	{ id: 'cs', name: 'Czech' },
+	{ id: 'nl', name: 'Dutch' },
+	{ id: 'sk', name: 'Slovak' },
+	{ id: 'ru', name: 'Russian' },
+	{ id: 'de', name: 'German' },
+	{ id: 'it', name: 'Italian' },
+	{ id: 'ko', name: 'Korean' },
+	{ id: 'pt-BR', name: 'Brazilian Portuguese' },
+	{ id: 'ar', name: 'Arabic' },
+	{ id: 'tr', name: 'Turkish' }
 ]);
